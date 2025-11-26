f334a83bdaca5244c8d677525a733e12da471376
Gea73 <geanccarlos2006@gmail.com>
2025-11-26 09:15:43 -0300

    theme saving localstorage

--- scripts/base.js
+++ scripts/base.js
@@ -1,15 +1,36 @@
-const changeTheme = document.querySelector('.change-theme');
+const changeTheme = document.querySelector(".change-theme");
+const savedTheme = localStorage.getItem("theme");
+ const root = document.documentElement;
 
-if(changeTheme){
-changeTheme.addEventListener("click", () => {
-  const root = document.documentElement;
-  if (root.classList.contains("light")) {
+if (savedTheme) {
+  if (savedTheme === "dark") {
     root.classList.remove("light");
     changeTheme.innerHTML = "☀ Change theme";
+    
+
   } else {
     root.classList.add("light");
-    changeTheme.innerHTML =
-      '<span style="font-weight: bold;">☾</span> Change theme';
+    changeTheme.innerHTML = '<span style="font-weight: bold;">☾</span> Change theme';
+    
   }
-});
 }
+
+if (changeTheme) {
+  changeTheme.addEventListener("click", () => {
+   
+
+    if (root.classList.contains("light")) {
+      root.classList.remove("light");
+      changeTheme.innerHTML = "☀ Change theme";
+      localStorage.setItem("theme", "dark");
+
+    } else {
+      root.classList.add("light");
+      changeTheme.innerHTML ='<span style="font-weight: bold;">☾</span> Change theme';
+      localStorage.setItem("theme", "light");
+    }
+
+  });
+}
+
+
