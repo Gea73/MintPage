7b414acb9d28a0a8460daff79f8f9412b01e8da0
Gea73 <geanccarlos2006@gmail.com>
2026-02-16 11:49:12 -0300

    Server changed to run with .env Port var instead of 5000

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
-
+const port = process.env.PORT || 5000;
 //start the server
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server running on port 5000");
 });
