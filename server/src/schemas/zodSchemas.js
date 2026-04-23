5c0990478305a72a0cd86f5e4dc36b9fab146e81
Gea73 <geanccarlos2006@gmail.com>
2026-04-23 13:20:41 -0300

    feat:zod input validation added

---
+++ server/src/schemas/zodSchemas.js
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+const userSchema = z.object({
+  username: z.string().min(3).max(25),
+  email: z.email(),
+  password: z
+    .string()
+    .min(8)
+    .max(50)
+    .regex(/[A-Z]/)
+    .regex(/[0-9]/)
+    .regex(/[a-z]/)
+    .regex(/[^A-Za-z0-9]/),
+});
+
+const resetPasswordSchema = z.object({
+  email: z.email(),
+  token: z.string().regex(/[^[a-fA-F0-9]{32}$]/),
+  password: z
+    .string()
+    .min(8)
+    .max(50)
+    .regex(/[A-Z]/)
+    .regex(/[0-9]/)
+    .regex(/[a-z]/)
+    .regex(/[^A-Za-z0-9]/),
+});
+
+const emailSchema = z.object({
+  email: z.email(),
+});
+
+export { userSchema, resetPasswordSchema, emailSchema };
