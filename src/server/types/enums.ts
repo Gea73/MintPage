509e466c01a05f05ca4de0b17eadee6104e2d8f6
Gea73 <geanccarlos2006@gmail.com>
2026-09-01 16:49:24 -0300

    feat:Enumerables added

---
+++ src/server/types/enums.ts
@@ -0,0 +1,4 @@
+export type UserStatus = "unverified" | "verified" | "suspended";
+export type MemberRole = "owner" | "admin" | "editor" | "reader";
+export type CardStatus = "pending" | "done";
+export type TokenStatus = "valid" | "expired" | "revoked";
