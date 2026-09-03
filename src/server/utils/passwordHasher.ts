70d8cbc6ab0d6a95e7fab090edba33495062d38b
Gea73 <geanccarlos2006@gmail.com>
2026-09-03 11:34:49 -0300

    security:Argon cost reduced to 19mb instead of 64mb

--- src/server/utils/passwordHasher.ts
+++ src/server/utils/passwordHasher.ts
@@ -3,7 +3,7 @@ export const passwordHasher = {
   hash: async function (password:string) {
     return await argon2id.hash(password, {
       type: argon2id.argon2id,
-      memoryCost: 64*1024,
+      memoryCost: 19 *1024,
       timeCost: 2,
       parallelism: 1,
     });
